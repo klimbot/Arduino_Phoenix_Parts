43f35749a50afd4c8caca6b8b4c7b0367c6e6092
Andrew <klimbot@gmail.com>
2019-12-15 22:18:39 +1100

    Fixing CI and build issues

--- Phoenix/AliPhoenix_Serial_Maestro/AliPhoenix_Serial_Maestro.ino
+++ Phoenix/AliPhoenix_Serial_Maestro/AliPhoenix_Serial_Maestro.ino
@@ -20,18 +20,13 @@
 //=============================================================================
 
 #define DEFINE_HEX_GLOBALS
-#if ARDUINO>99
 #include <Arduino.h>
-#else
-#endif
-//#include <Wire.h>
 #include <EEPROM.h>
 #include <PS2X_lib.h>
 #include <SoftwareSerial.h>
 
-#include "Hex_CFG.h"
+#include "Hex_Cfg.h"
 #include <Phoenix.h>
 #include <Phoenix_Input_Serial.h>
 #include <Phoenix_Driver_Maestro.h>
 #include <Phoenix_Code.h>
-
