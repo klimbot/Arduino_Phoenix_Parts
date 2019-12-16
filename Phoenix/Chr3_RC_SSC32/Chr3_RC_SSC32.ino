3696dfa05a3290653a3a804b1c17f5d0cffd1886
Andrew <klimbot@gmail.com>
2019-12-16 16:29:40 +1100

    Fixed compilation issue

--- Phoenix/Chr3_RC_SSC32/Chr3_RC_SSC32.ino
+++ Phoenix/Chr3_RC_SSC32/Chr3_RC_SSC32.ino
@@ -21,17 +21,13 @@
 //=============================================================================
 
 #define DEFINE_HEX_GLOBALS
-#if ARDUINO>99
 #include <Arduino.h>
-#else
-#endif
 #include <Wire.h>
 #include <EEPROM.h>
 //#include <SoftwareSerial.h>
 
-#include "Hex_CFG.h"
+#include "Hex_Cfg.h"
 #include <Phoenix.h>
 #include <Phoenix_Input_RC.h>
 #include <Phoenix_Driver_SSC32.h>
 #include <Phoenix_Code.h>
-
